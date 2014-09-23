ce132ea36102280e2954ae38663a47d4030f7d06
synchronika <obyrexsus@gmail.com>
2014-09-23 16:17:56 +0300

    Updated
    Updated

--- src/src/mat/sn/SocialNetwork.java
+++ src/src/mat/sn/SocialNetwork.java
@@ -1,12 +1,16 @@
-package mat.sn;
+package mat;
 
+ 
+import java.io.IOException;
 import java.util.List;
 
-public abstract class SocialNetwork extends SocialNetworks {
+public abstract class SocialNetwork {
     //Methods that all the social networks must implement
     abstract List<String> getContacts(String accessToken);
     abstract boolean shareByMail(String urlMatt, String[] contacts, String accessToken);
     abstract TokenData retrieveToken(String authCode);
     abstract boolean refreshToken(TokenData token);
     abstract String[] getApplicationData();
+    abstract void setMatCalendar(List<Matt> matts, String accessToken);
+    abstract List<Boolean> getSlots(MattData interval, String accessToken) throws IOException;
 }
